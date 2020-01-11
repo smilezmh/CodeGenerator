3ec234677a3b0870ba7a206e83f15d555ef8bd9d
smilezmh <544635063@qq.com>
2020-01-11 08:37:33 +0800

    first commit

--- src/main/java/cmtech/soft/equipment/utils/MyStrTool.java
+++ src/main/java/cmtech/soft/equipment/utils/MyStrTool.java
@@ -1,10 +1,10 @@
 package cmtech.soft.equipment.utils;
 
 public class MyStrTool {
-    public static boolean isNullOrEmpty(String str){
-        if(str==null||str.trim().equals("")){
-            return  true;
-        }else {
+    public static boolean isNullOrEmpty(String str) {
+        if (str == null || str.trim().equals("") || str.isEmpty()) {
+            return true;
+        } else {
             return false;
         }
     }
