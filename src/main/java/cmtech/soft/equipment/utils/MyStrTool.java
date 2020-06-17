62b3a4d0472990d98e4b2bfd10b580aac81ac848
menghui.zhao@cmtech.com <menghui.zhao@cmtech.com>
2020-06-17 15:31:46 +0800

    bug

--- src/main/java/cmtech/soft/equipment/utils/MyStrTool.java
+++ src/main/java/cmtech/soft/equipment/utils/MyStrTool.java
@@ -416,6 +416,23 @@ public class MyStrTool {
     public static int getNextIndexSymbolInStr(int lastTimesIndex, String str, String symbol) {
         return str.indexOf(symbol, lastTimesIndex + 1);
     }
+
+    //首字母转小写
+    public static String toLowerCaseFirstOne(String s)
+    {
+        if(Character.isLowerCase(s.charAt(0)))
+            return s;
+        else
+            return (new StringBuilder()).append(Character.toLowerCase(s.charAt(0))).append(s.substring(1)).toString();
+    }
+    //首字母转大写
+    public static String toUpperCaseFirstOne(String s)
+    {
+        if(Character.isUpperCase(s.charAt(0)))
+            return s;
+        else
+            return (new StringBuilder()).append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).toString();
+    }
 }
 
 /**
