54dbeaa399be015b98968c1c436b4b3f042ce233
menghui.zhao@cmtech.com <menghui.zhao@cmtech.com>
2020-09-11 13:37:21 +0800

    -

--- src/main/java/cmtech/soft/equipment/utils/MyListUtil.java
+++ src/main/java/cmtech/soft/equipment/utils/MyListUtil.java
@@ -4,39 +4,53 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 集合工具类
+ *
  * @param <T> 范型
  */
-public class MyListUtil<T> {
+public class MyListUtil<T, U> {
+
+    public static <T, U> List<U> getListTU(List<T> list, Function<? super T, ? extends U> iConvert) {
+        List<U> returnList = null;
+
+        if (!isNullOrEmpty(list)) {
+            returnList = list.stream().map(iConvert).collect(Collectors.toList());
+        }
+
+        return returnList;
+    }
+
     // 集合去重
-    public static <T> List<T> distinct(List<T> list){
+    public static <T> List<T> distinct(List<T> list) {
         Set hashSet = new HashSet(list);
-        List<T> newList=new ArrayList<>();
+        List<T> newList = new ArrayList<>();
         newList.addAll(hashSet);
         return newList;
     }
 
     // 判断集合是否为空
-    public static <T> boolean isNullOrEmpty(List<T> list){
-       boolean flag=true;
+    public static <T> boolean isNullOrEmpty(List<T> list) {
+        boolean flag = true;
 
-        if(list!=null&&!list.isEmpty()){
-            flag=false;
+        if (list != null && !list.isEmpty()) {
+            flag = false;
         }
 
         return flag;
     }
 
     // 查找重复的数据集合
-    public static <T> List<T> findRepeatData(List<T> list){
-        List<T> newList=new ArrayList<>();
-        List<T> distinctList =distinct(list);
+    public static <T> List<T> findRepeatData(List<T> list) {
+        List<T> newList = new ArrayList<>();
+        List<T> distinctList = distinct(list);
 
-        if(list!=null&&distinctList!=null&&(list.size()>distinctList.size())){
-            for(T t:distinctList){
-                if(findTimesInList(list, t)>1){
+        if (list != null && distinctList != null && (list.size() > distinctList.size())) {
+            for (T t : distinctList) {
+                if (findTimesInList(list, t) > 1) {
                     newList.add(t);
                 }
             }
@@ -46,26 +60,26 @@ public class MyListUtil<T> {
     }
 
     // 查找是否有重复数据
-    public static <T> boolean isHasRepeatData(List<T> list){
-        boolean flag=false;
+    public static <T> boolean isHasRepeatData(List<T> list) {
+        boolean flag = false;
 
-        List<T> distinctList =distinct(list);
+        List<T> distinctList = distinct(list);
 
-        if(list!=null&&distinctList!=null&&(list.size()>distinctList.size())){
-            flag=true;
+        if (list != null && distinctList != null && (list.size() > distinctList.size())) {
+            flag = true;
         }
 
         return flag;
     }
 
     // 找出一个元素在集合中出现的次数
-    public static <T> Integer findTimesInList(List<T> list,T t){
-        Integer times=0;
+    public static <T> Integer findTimesInList(List<T> list, T t) {
+        Integer times = 0;
 
-        if(!isNullOrEmpty(list)&&t!=null){
+        if (!isNullOrEmpty(list) && t != null) {
 
-            for(T obj:list){
-                if(t.equals(obj)){
+            for (T obj : list) {
+                if (t.equals(obj)) {
                     times++;
                 }
             }
