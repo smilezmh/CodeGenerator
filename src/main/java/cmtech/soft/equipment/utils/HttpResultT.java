52682facfc6cffde6364af8cb990d1f5aba73050
menghui.zhao@cmtech.com <menghui.zhao@cmtech.com>
2020-10-27 13:29:51 +0800

    -

--- src/main/java/cmtech/soft/equipment/utils/HttpResultT.java
+++ src/main/java/cmtech/soft/equipment/utils/HttpResultT.java
@@ -3,43 +3,72 @@ package cmtech.soft.equipment.utils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
-@ApiModel(value="返回前端对象", description="返回前端对象")
+@ApiModel(value = "返回前端对象", description = "返回前端对象")
 @Data
-@Component
-@Scope("prototype")
 public class HttpResultT<T> {
 
     @ApiModelProperty(value = "返回http码")
-    private int code;
+    private int code = HttpStatus.SC_OK;
 
     @ApiModelProperty(value = "返回消息")
-    private String msg;
+    private String msg = "操作成功";
 
     @ApiModelProperty(value = "返回数据")
     private T data;
 
-    public HttpResultT<T> ok(T t){
-        this.code=HttpStatus.SC_OK;
-        this.msg="返回成功";
-        this.data=t;
+    public static <T> HttpResultT<T> create() {
+        HttpResultT<T> httpResultT = new HttpResultT();
+        return httpResultT;
+    }
+
+    public HttpResultT<T> build(T t) {
+        this.data = t;
         return this;
     }
 
-    public HttpResultT<T> error(String msg){
-        this.code=HttpStatus.SC_Allert;
-        this.msg=msg;
-        this.data=null;
+    public HttpResultT<T> build(String msg) {
+        this.msg = msg;
         return this;
     }
 
-    public HttpResultT<T> empty(String msg){
-        this.code=HttpStatus.SC_NO_CONTENT;
-        this.msg="返回为空";
-        this.data=null;
+    public HttpResultT<T> build(int code) {
+        this.code = code;
         return this;
     }
 
+    public HttpResultT<T> ok(T t) {
+        this.code = HttpStatus.SC_OK;
+        this.msg = "操作成功";
+        this.data = t;
+        return this;
+    }
+
+    public HttpResultT<T> ok(T t, String msg) {
+        this.code = HttpStatus.SC_OK;
+        this.msg = msg;
+        this.data = t;
+        return this;
+    }
+
+    public HttpResultT<T> error(String msg) {
+        this.code = HttpStatus.SC_Allert;
+        this.msg = msg;
+        this.data = null;
+        return this;
+    }
+
+    public HttpResultT<T> error() {
+        this.code = HttpStatus.SC_Allert;
+        this.msg = "操作失败";
+        this.data = null;
+        return this;
+    }
+
+    public HttpResultT<T> empty() {
+        this.code = HttpStatus.SC_NO_CONTENT;
+        this.msg = "返回为空";
+        this.data = null;
+        return this;
+    }
 }
